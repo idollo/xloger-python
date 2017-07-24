2c23fe73a707aaaed13a006d58acad88a2a10302
huangjunlin <stone58@qq.com>
2017-07-24 17:02:03 +0800

    fixed: log feature

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='xloger',
-    version="1.1.4",
+    version="1.1.5",
     url='https://github.com/idollo/xloger-python.git',
     license='MIT',
     author='idollo',
